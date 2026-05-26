50b50491588c8a44fcdefb523aee22671e67cb9e
Sumit-Pathrabe <sumit.pathrabe17@gmail.com>
2026-05-26 11:20:08 +0530

    Added connection to postgres

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-import psycopg2
+import psycopg2 as psycopg2
 from psycopg2.extras import RealDictCursor
 
 app = FastAPI()
+# PostgreSQL Connection
+conn = psycopg2.connect(
+    host="localhost",
+    database="library_db",
+    user="postgres",
+    password="YOUR_PASSWORD",
+    cursor_factory=RealDictCursor
+)
